328dd1b735b60d2f495957272a5ec2879480558c
Ricardo Serrano <ricardo.ss@live.com>
2022-01-27 15:09:38 -0600

    feat: Add POJO for User

---
+++ src/main/java/com/serrano/app/helpdesk/domain/User.java
@@ -0,0 +1,12 @@
+package com.serrano.app.helpdesk.domain;
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean locked;
+    private Boolean enabled;
+}
