d1656a5e928349a24120a4cb07142263c13b4a81
Ricardo Serrano <ricardo.ss@live.com>
2022-01-28 15:34:27 -0600

    feat: Override method to handle validation annotations

--- src/main/java/com/serrano/app/helpdesk/exception/ControllerAdvisor.java
+++ src/main/java/com/serrano/app/helpdesk/exception/ControllerAdvisor.java
@@ -2,8 +2,10 @@ package com.serrano.app.helpdesk.exception;
 
 import java.time.ZonedDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -21,5 +23,11 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
             status,
             ZonedDateTime.now());
         return new ResponseEntity<>(exception,status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
     }
 }
