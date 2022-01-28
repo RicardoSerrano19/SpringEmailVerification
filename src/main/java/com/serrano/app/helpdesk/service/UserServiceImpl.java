56d80aa8225e733d00555b593f51e5dff46cedd6
Ricardo Serrano <ricardo.ss@live.com>
2022-01-28 13:13:11 -0600

    feat: Implement UserService methods

---
+++ src/main/java/com/serrano/app/helpdesk/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.serrano.app.helpdesk.service;
+
+import java.util.List;
+import com.serrano.app.helpdesk.domain.User;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public User save(User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User get(String username) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
