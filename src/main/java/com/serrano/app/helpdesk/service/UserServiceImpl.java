223dc94bda8b15e02ed7835857f7bed7c54dbab1
Ricardo Serrano <ricardo.ss@live.com>
2022-01-28 14:53:57 -0600

    feat: Use repository to execute queries

--- src/main/java/com/serrano/app/helpdesk/service/UserServiceImpl.java
+++ src/main/java/com/serrano/app/helpdesk/service/UserServiceImpl.java
@@ -1,25 +1,31 @@
 package com.serrano.app.helpdesk.service;
 
 import java.util.List;
+import java.util.ArrayList;
 import com.serrano.app.helpdesk.domain.User;
+import com.serrano.app.helpdesk.repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepo;
 
     @Override
     public User save(User user) {
-        // TODO Auto-generated method stub
-        return null;
+        return userRepo.save(user);
     }
 
     @Override
     public User get(String username) {
-        // TODO Auto-generated method stub
-        return null;
+        return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        return userRepo.findAll();
     }
 }
