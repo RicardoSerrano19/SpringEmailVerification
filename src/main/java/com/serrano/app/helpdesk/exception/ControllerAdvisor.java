b2173fe3819e5036a867fd5d48c581898f2fbe7f
Ricardo Serrano <ricardo.ss@live.com>
2022-01-28 15:32:46 -0600

    feat: Add method to handle UserNotFoundException

--- src/main/java/com/serrano/app/helpdesk/exception/ControllerAdvisor.java
+++ src/main/java/com/serrano/app/helpdesk/exception/ControllerAdvisor.java
@@ -1,9 +1,25 @@
 package com.serrano.app.helpdesk.exception;
 
+import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerAdvisor extends ResponseEntityExceptionHandler{
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        APIException exception = new APIException(
+            ex.getMessage(), 
+            ex, 
+            status,
+            ZonedDateTime.now());
+        return new ResponseEntity<>(exception,status);
+    }
 }
