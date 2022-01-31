4bb104f176ac892c1615c78d962f8671e92ad7d3
Ricardo Serrano <ricardo.ss@live.com>
2022-01-31 14:41:54 -0600

    fix: Return Optional

--- src/main/java/com/serrano/app/helpdesk/repository/UserRepository.java
+++ src/main/java/com/serrano/app/helpdesk/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.serrano.app.helpdesk.repository;
 
+import java.util.Optional;
+
 import com.serrano.app.helpdesk.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
