5b2b03c72829aad25cdc8751fa64e82a44c982c5
Ricardo Serrano <ricardo.ss@live.com>
2022-01-31 13:09:23 -0600

    feat: Add Get method for findAll

--- src/main/java/com/serrano/app/helpdesk/controller/UserController.java
+++ src/main/java/com/serrano/app/helpdesk/controller/UserController.java
@@ -9,7 +9,9 @@ import com.serrano.app.helpdesk.service.UserServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,13 @@ public class UserController {
     UserServiceImpl userService;
 
     @PostMapping("/users")
-    public ResponseEntity<User> getUsers(@Valid @RequestBody User user){
-        return new ResponseEntity<User>(userService.save(user), HttpStatus.OK);
+    public ResponseEntity<User> saveUser(@Valid @RequestBody User user){
+        return new ResponseEntity<User>(userService.save(user), HttpStatus.CREATED);
     } 
+
+    @GetMapping(path = "/users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> findAll(){
+        return new ResponseEntity<List<User>>(userService.findAll(), HttpStatus.OK);
+    }
+
 }
