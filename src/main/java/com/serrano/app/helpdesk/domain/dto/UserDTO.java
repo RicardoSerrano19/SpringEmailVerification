4b1f9171a00476949dbcd8e0a8998312fe04d93b
Ricardo Serrano <ricardo.ss@live.com>
2022-01-31 14:19:00 -0600

    feat: Add DTO to use around the network

---
+++ src/main/java/com/serrano/app/helpdesk/domain/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.serrano.app.helpdesk.domain.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserDTO implements Serializable{
+    
+    @NotEmpty(message = "firstName should not be empty")
+    @Size(min = 5, max = 200, message 
+      = "firstName must be between 5 and 100 characters")
+    private String firstName;
+    @NotEmpty(message = "lastName should not be empty")
+    @Size(min = 5, max = 200, message 
+    = "lastName must be between 5 and 100 characters")
+    private String lastName;
+    @NotEmpty(message = "email should not be empty")
+    @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+    message = "email must be valid")
+    private String email;
+    @NotEmpty(message = "password should not be empty")
+    @Size(min = 5, max = 100, message 
+    = "password must be between 8 and 100 characters")
+    private String password;
+}
