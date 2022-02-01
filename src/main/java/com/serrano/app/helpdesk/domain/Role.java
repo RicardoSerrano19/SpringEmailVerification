f218e8f0c3717111391e5be4504f7c0b6b265722
Ricardo Serrano <ricardo.ss@live.com>
2022-02-01 14:06:44 -0600

    feat: Add role entity

--- src/main/java/com/serrano/app/helpdesk/domain/Role.java
+++ src/main/java/com/serrano/app/helpdesk/domain/Role.java
@@ -1,9 +1,13 @@
 package com.serrano.app.helpdesk.domain;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,4 +22,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<User>();
 }
