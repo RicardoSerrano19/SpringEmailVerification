aa45760fe9cad15077b98669b83b0236d0fecd1c
Ricardo Serrano <ricardo.ss@live.com>
2022-02-01 14:10:28 -0600

    feat: Add DTO to handle List of users with rol

---
+++ src/main/java/com/serrano/app/helpdesk/domain/dto/UserRoleDTO.java
@@ -0,0 +1,14 @@
+package com.serrano.app.helpdesk.domain.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleDTO {
+    List<UserDTO> userRole;
+}
