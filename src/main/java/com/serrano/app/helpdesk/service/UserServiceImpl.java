074c71e3a99d2c81a91c4a4cf6b2f848edf9b55d
Ricardo Serrano <ricardo.ss@live.com>
2022-01-31 20:39:00 -0600

    feat: Add business logic to getByEmail

--- src/main/java/com/serrano/app/helpdesk/service/UserServiceImpl.java
+++ src/main/java/com/serrano/app/helpdesk/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.serrano.app.helpdesk.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.ArrayList;
 import com.serrano.app.helpdesk.domain.User;
+import com.serrano.app.helpdesk.domain.dto.UserDTO;
+import com.serrano.app.helpdesk.exception.EmailNotFoundException;
 import com.serrano.app.helpdesk.repository.UserRepository;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,19 +17,28 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
-    public User save(User user) {
-        return userRepo.save(user);
+    public UserDTO save(UserDTO user) {
+        User entitySaved = userRepo.save(mapper.map(user, User.class));
+        UserDTO userMapped = mapper.map(entitySaved, UserDTO.class);
+        return userMapped;
     }
 
     @Override
-    public User get(String email) {
-        return userRepo.findByEmail(email);
+    public UserDTO get(String email) {
+        Optional<User> user = userRepo.findByEmail(email);
+        if(!user.isPresent()) throw new EmailNotFoundException(email);
+        UserDTO userMapped = mapper.map(user.get(), UserDTO.class);
+        return userMapped;
     }
 
     @Override
-    public List<User> findAll() {
-        return userRepo.findAll();
+    public List<UserDTO> findAll() {
+        //List<User> entity = userRepo.findAll();
+        List<UserDTO> list = new ArrayList<>();
+        return list;
     }
 }
