dfe9daea48f4674e2874b07f9413190994561ff4
Ricardo Serrano <ricardo.ss@live.com>
2022-02-02 12:34:26 -0600

    feat: Add repository for role

--- src/main/java/com/serrano/app/helpdesk/repository/RoleRepository.java
+++ src/main/java/com/serrano/app/helpdesk/repository/RoleRepository.java
@@ -3,9 +3,12 @@ package com.serrano.app.helpdesk.repository;
 import java.util.Optional;
 
 import com.serrano.app.helpdesk.domain.Role;
+import com.serrano.app.helpdesk.enums.RoleName;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
-    Optional<Role> findByName(String name);
+    Optional<RoleName> findByName(RoleName roleName);
 }
