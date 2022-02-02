3d325089c01aa8655007183e624474b511217d80
Ricardo Serrano <ricardo.ss@live.com>
2022-02-02 12:35:14 -0600

    feat: Verify if role exist

--- src/main/java/com/serrano/app/helpdesk/enums/RoleName.java
+++ src/main/java/com/serrano/app/helpdesk/enums/RoleName.java
@@ -3,5 +3,13 @@ package com.serrano.app.helpdesk.enums;
 public enum RoleName {
     ROLE_ADMIN,
     ROLE_SUPPORT,
-    ROLE_USER
+    ROLE_USER;
+
+    public static RoleName asRoleName(String str){
+        for(RoleName role: RoleName.values()){
+            if(role.name().equals(str))
+                return role;
+        }
+        return null;
+    }
 }
