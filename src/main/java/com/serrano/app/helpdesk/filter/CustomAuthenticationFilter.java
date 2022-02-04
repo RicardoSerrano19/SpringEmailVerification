2725068b7736651ece7f5c09e2769ea4f614016c
Ricardo Serrano <ricardo.ss@live.com>
2022-02-04 15:30:04 -0600

    feat: Add Authentication Filter and check the attempt

---
+++ src/main/java/com/serrano/app/helpdesk/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.serrano.app.helpdesk.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+       String username = request.getParameter("username");
+       String password = request.getParameter("password");
+       log.info("Username is : {} and password is: {}", username, password);
+       UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+       return authenticationManager.authenticate(authenticationToken);
+    }
+}
