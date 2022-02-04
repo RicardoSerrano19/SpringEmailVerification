b4f019de07fbb1eea2858de1df97e052e7df4809
Ricardo Serrano <ricardo.ss@live.com>
2022-02-04 15:30:18 -0600

    feat: Add filter to http

--- src/main/java/com/serrano/app/helpdesk/security/SecurityConfig.java
+++ src/main/java/com/serrano/app/helpdesk/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.serrano.app.helpdesk.security;
 
+import com.serrano.app.helpdesk.filter.CustomAuthenticationFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -22,7 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers("/login", "/api/v1/token/refresh").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/users").permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
 
     @Override
