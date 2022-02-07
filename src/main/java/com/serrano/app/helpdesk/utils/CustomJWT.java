d9190d08c02124bcb6ec9063c555be9e9949ae51
Ricardo Serrano <ricardo.ss@live.com>
2022-02-07 13:08:09 -0600

    feat: Add methods to create JWT

---
+++ src/main/java/com/serrano/app/helpdesk/utils/CustomJWT.java
@@ -0,0 +1,42 @@
+package com.serrano.app.helpdesk.utils;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import org.springframework.security.core.userdetails.User;
+
+public class CustomJWT {
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256("9a&t+Gg?YC$9cK!E".getBytes());
+
+    public static String create(User user, int minutes, String issuer, String claim){
+        Date date = new Date(System.currentTimeMillis() + (minutes * 60000));
+        String username = user.getUsername();
+        return JWT.create()
+                .withSubject(username)
+                .withExpiresAt(date)
+                .withIssuer(issuer)
+                .withClaim(claim, getClaimValues(user))
+                .sign(ALGORITHM);
+    }
+
+    public static String create(User user, int minutes, String issuer){
+        Date date = new Date(System.currentTimeMillis() + (minutes * 60000));
+        String username = user.getUsername();
+        return JWT.create()
+                .withSubject(username)
+                .withExpiresAt(date)
+                .withIssuer(issuer)
+                .sign(ALGORITHM);
+    }
+
+
+    private static List<String> getClaimValues(User user){
+        return user.getAuthorities().stream()
+            .map(role -> role.getAuthority())
+            .collect(Collectors.toList());
+    }
+}
