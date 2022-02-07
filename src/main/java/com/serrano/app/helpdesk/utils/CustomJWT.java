bb616e2b8f8652356789539fbe68c9491aad9de5
Ricardo Serrano <ricardo.ss@live.com>
2022-02-07 13:45:37 -0600

    feat: Create authentication token

--- src/main/java/com/serrano/app/helpdesk/utils/CustomJWT.java
+++ src/main/java/com/serrano/app/helpdesk/utils/CustomJWT.java
@@ -1,14 +1,19 @@
 package com.serrano.app.helpdesk.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 public class CustomJWT {
@@ -38,6 +43,18 @@ public class CustomJWT {
     public static DecodedJWT decode(String token){
         JWTVerifier verifier = JWT.require(ALGORITHM).build();
         return verifier.verify(token);
+    }
+
+    public static UsernamePasswordAuthenticationToken createAuthenticationToken(DecodedJWT decoded){
+        String username = decoded.getSubject();
+        String[] roles = decoded.getClaim("roles").asArray(String.class);
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        
+        Stream.of(roles).forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role));
+        });
+        
+        return new UsernamePasswordAuthenticationToken(username, null, authorities);
     }
 
 
