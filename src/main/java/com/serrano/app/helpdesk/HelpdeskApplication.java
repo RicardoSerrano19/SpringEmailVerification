a3cde4b2562098e9022a7960004c3df20249036e
Ricardo Serrano <ricardo.ss@live.com>
2022-02-09 14:39:00 -0600

    feat: Move beans to special component

--- src/main/java/com/serrano/app/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/serrano/app/helpdesk/HelpdeskApplication.java
@@ -1,27 +1,13 @@
 package com.serrano.app.helpdesk;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HelpdeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
 	}
 
 }
