ed9752d1678245c6238cb0ce9845207c63d551d4
Ricardo Serrano <ricardo.ss@live.com>
2022-02-09 14:38:30 -0600

    feat: Add Component to handle beans

---
+++ src/main/java/com/serrano/app/helpdesk/bean/BeanUtilities.java
@@ -0,0 +1,51 @@
+package com.serrano.app.helpdesk.bean;
+
+import java.util.Properties;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanUtilities {
+    
+    @Autowired
+	private Environment env;
+
+    @Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		String host = env.getProperty("spring.mail.host");
+		String port = env.getProperty("spring.mail.port");
+		String username = env.getProperty("spring.mail.username");
+		String password = env.getProperty("spring.mail.password");
+		mailSender.setHost(host);
+		mailSender.setPort(Integer.parseInt(port));
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+		
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+		
+		return mailSender;
+	}
+}
